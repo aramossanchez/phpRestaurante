100620ef3b1cfcfd3689b0b6792ea483d2563553
Armando <armandoramossanchez@gmail.com>
2021-12-07 19:09:50 +0100

    cambio acceso a base de datos de heroku

--- database/dbconnect.php
+++ database/dbconnect.php
@@ -1,6 +1,6 @@
 <?php 
 function conectar(){
-    $mysqli = new mysqli("127.0.0.1:6666", "root" ,"root123", "restaurante");
+    $mysqli = new mysqli("eu-cdbr-west-01.cleardb.com", "b234c37892b334" ,"930e6a58", "restaurante");
     //control de errores
     if($mysqli->connect_errno){
         echo "Fallo la conexión (' . $mysqli->connect_errno . ') " . $mysqli->connect_errno;
